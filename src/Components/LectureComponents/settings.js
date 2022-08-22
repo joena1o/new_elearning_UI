c1c1ce90e8fd7347d8120e17286097ef1a37002a
Hyefur Jonathan Balami <jonathanhyefur@gmail.com>
2022-08-22 10:52:38 +0100

    token changed

--- src/Components/LectureComponents/settings.js
+++ src/Components/LectureComponents/settings.js
@@ -1,8 +1,8 @@
 import { createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
 
-const appId = "ba0c7397a38640cca781bc8dbcd73063";
-const token  = "007eJxTYOAwXSQbp7u/OlnXszjQqPX2/u+57MYtrcpKTz2PTWq5sV6BISnRINnc2NI80djCzMQgOTnR3MIwKdkiJSk5xdzYwMw47ihD8l0xpuSwTWosjAwQCOKzMOQmZuYxMAAAwPsebA==";
-
+const appId = "2fa3d707afcf41d9834f7bc71caaa263";
+//const token  = "007eJxTYOAwXSQbp7u/OlnXszjQqPX2/u+57MYtrcpKTz2PTWq5sV6BISnRINnc2NI80djCzMQgOTnR3MIwKdkiJSk5xdzYwMw47ihD8l0xpuSwTWosjAwQCOKzMOQmZuYxMAAAwPsebA==";
+const token = null;
 export const config = { mode: "rtc", codec: "vp8", appId: appId, token: token};
 export const useClient = createClient(config);
 export const useMicrophoneAndCameraTracks = createMicrophoneAndCameraTracks();
